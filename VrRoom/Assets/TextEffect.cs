f87578521872ecf23371acbbc237f7695c905f6b
anshurw <anshurw@gmail.com>
2023-06-11 16:19:10 +0530

    update

---
+++ VrRoom/Assets/TextEffect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class TextEffect : MonoBehaviour
+{
+    public string TextToshow;
+    public float typespeed = 5;
+    bool writing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextToshow = GetComponent<TextMeshProUGUI>().text;
+        GetComponent<TextMeshProUGUI>().text = "";
+        StartCoroutine(WriteText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator WriteText()
+    {
+        foreach (var item in TextToshow)
+        {
+            GetComponent<TextMeshProUGUI>().text += item;
+            yield return new WaitForSeconds(typespeed);
+           
+        }
+        
+    }
+}
